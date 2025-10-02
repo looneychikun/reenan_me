075dd9869a1ea1d968ab54fe517963e26aabc83c
LoONeYChIKuN <andrew@reenan.me>
2025-10-01 19:48:45 -0700

    implement the blog

---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+
+import { glob, file } from 'astro/loaders';
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*{.md,mdx}", base: './src/content/blog' }),
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    collection: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    updatedDate: z.date().optional()
+  })
+});
+
+export const collections = { blog };
